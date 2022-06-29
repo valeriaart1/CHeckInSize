ba66a0941648f0207a72e43ac4fea4d5d2f8248c
valeriaart1 <valeriabykhov@mail.ru>
2022-06-29 22:51:27 +0300

    Initial Commit

---
+++ CHeckInSize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHeckInSize
+//
+//  Created by Валерия Артемьева on 29.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
